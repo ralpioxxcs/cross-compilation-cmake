3f335bf5b8765f1ccac4cb93eeefcaf9b739b774
ralpioxxcs <tmfmfkal1123@naver.com>
2022-07-17 23:52:04 +0900

    initial commit

---
+++ aarch64.toolchain.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_SYSROOT /home/user/sysroot)
+#set(CMAKE_STAGING_PREFIX /home/user/stage)
+
+set(tools /usr/bin/aarch64-linux-gnu)
+set(CMAKE_C_COMPILER ${tools}-gcc-8)
+set(CMAKE_CXX_COMPILER ${tools}-g++-8)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
